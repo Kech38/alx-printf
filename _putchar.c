19eb00c40293adfbcd4581ba1eaff722d2c52ba4
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-22 22:32:52 +0100

    Simplified the comments and removed extra spaces

--- _putchar.c
+++ _putchar.c
@@ -2,14 +2,13 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: The character to write
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, returns -1.
  */
 
 int _putchar(char c)
-
 {
 return (write(1, &c, 1));
 }
