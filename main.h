6227d4a2ea141ff98b4199bc8148d5604c23967d
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-22 23:58:37 +0100

    added the struct

--- main.h
+++ main.h
@@ -11,4 +11,16 @@ int print_int(va_list arg);
 int print_char(va_list arg);
 int print_str(va_list arg);
 
+/**
+ * struct printStruct - structure definition of a printTypeStruct
+ * @type: data type
+ * @printer: function to print
+ */
+typedef struct printStruct
+{
+char *type;
+int (*printer)(va_list);
+} printStruct;
+
+
 #endif
