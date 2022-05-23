3e3a94015d054b76364543d9f2897ee1ca88f2cf
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-23 03:49:17 +0100

    Added more header

--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
