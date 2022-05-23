4fbdf997d498bc7e68fa5e05a49b871b6cb5c86a
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-23 04:01:33 +0100

    Added print

--- main.h
+++ main.h
@@ -12,6 +12,7 @@ int _printf(const char *format, ...);
 int print_int(va_list arg);
 int print_char(va_list arg);
 int print_str(va_list arg);
+int _print(const char *format, va_list args);
 
 /**
  * struct printStruct - structure definition of a printTypeStruct
